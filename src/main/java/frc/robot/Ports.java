7a0c08de54daa468048fced5a6f9129a6e80ee7a
braydensilzell <144178170+braydensilzell@users.noreply.github.com>
2026-01-31 14:20:38 -0700

    chassis working

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -8,14 +8,14 @@ public final class Ports {
     public static final CANBus kCANivoreCANBus = new CANBus("main");
 
     // Talon FX IDs
-    public static final int kIntakePivot = 10;
-    public static final int kIntakeRollers = 11;
-    public static final int kFloor = 12;
-    public static final int kFeeder = 13;
-    public static final int kShooterLeft = 14;
-    public static final int kShooterMiddle = 15;
-    public static final int kShooterRight = 16;
-    public static final int kHanger = 18;
+    public static final int kIntakePivot = 13;
+    public static final int kIntakeRollers = 14;
+    public static final int kFloor = 15;
+    public static final int kFeeder = 16;
+    public static final int kShooterLeft = 17;
+    public static final int kShooterMiddle = 18;
+    public static final int kShooterRight = 19;
+    public static final int kHanger = 20;
 
     // PWM Ports
     public static final int kHoodLeftServo = 3;
